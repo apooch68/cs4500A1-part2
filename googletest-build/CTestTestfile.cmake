1887721bd8cc6b5fe6a8d4d27d17eb76da9f76f1
EricXuZh <ericxu@ericxu-VirtualBox.(none)>
2020-01-29 22:30:20 -0500

    removed more unnecessary files

--- googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ericxu/Documents/cs4500/cs4500A1-part2/googletest-src
-# Build directory: /home/ericxu/Documents/cs4500/cs4500A1-part2/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ericxu/Documents/cs4500/cs4500A1-part2/googletest-src/googletest
-# Build directory: /home/ericxu/Documents/cs4500/cs4500A1-part2/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
